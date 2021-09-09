6ef4afcb510a97b2c8cef8bbe8fabf501bb0b0e2
Suprememajor <ndipnobertetta@gmail.com>
2021-09-09 05:45:23 +0100

    added PetTypeService. Closes #14

---
+++ src/main/java/com/supremecorp/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package com.supremecorp.sfgpetclinic.services;
+
+import com.supremecorp.sfgpetclinic.models.PetType;
+
+/**
+ * Created by suprememajor on the 9/9/21
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ src/main/java/com/supremecorp/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,38 @@
+package com.supremecorp.sfgpetclinic.services.map;
+
+import com.supremecorp.sfgpetclinic.models.PetType;
+import com.supremecorp.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by suprememajor on the 9/9/21
+ */
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
