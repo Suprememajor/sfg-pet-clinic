a81cb3e6c0b953944cdfbac7ba2d7b0f1af3eeb5
Suprememajor <ndipnobertetta@gmail.com>
2021-09-09 06:35:51 +0100

    added specialty Map service Impl. Closes #16

---
+++ src/main/java/com/supremecorp/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,9 @@
+package com.supremecorp.sfgpetclinic.services;
+
+import com.supremecorp.sfgpetclinic.models.Specialty;
+
+/**
+ * Created by suprememajor on the 9/9/21
+ */
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ src/main/java/com/supremecorp/sfgpetclinic/services/map/SpecialtyServiceMAp.java
@@ -0,0 +1,38 @@
+package com.supremecorp.sfgpetclinic.services.map;
+
+import com.supremecorp.sfgpetclinic.models.Specialty;
+import com.supremecorp.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by suprememajor on the 9/9/21
+ */
+@Service
+public class SpecialtyServiceMAp extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
