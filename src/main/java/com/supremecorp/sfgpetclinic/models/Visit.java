91cfd6341f4910abf0d2e899898f622844b2d2a4
Suprememajor <ndipnobertetta@gmail.com>
2021-09-10 17:28:50 +0100

    Convert Visit to JPA Entity. Closes #23

--- src/main/java/com/supremecorp/sfgpetclinic/models/Visit.java
+++ src/main/java/com/supremecorp/sfgpetclinic/models/Visit.java
@@ -1,13 +1,21 @@
 package com.supremecorp.sfgpetclinic.models;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by suprememajor on the 9/9/21
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
