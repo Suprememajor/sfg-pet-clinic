76889cd1c29c821daa14fc88ab7271d294d327d4
Suprememajor <ndipnobertetta@gmail.com>
2021-09-10 18:17:36 +0100

    Created Spring Data JPA Service for Vet Specialties. Closes #29

---
+++ src/main/java/com/supremecorp/sfgpetclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,50 @@
+package com.supremecorp.sfgpetclinic.services.springdatajpa;
+
+import com.supremecorp.sfgpetclinic.models.Specialty;
+import com.supremecorp.sfgpetclinic.repositories.SpecialtyRepository;
+import com.supremecorp.sfgpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by suprememajor on the 9/10/21
+ */
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
