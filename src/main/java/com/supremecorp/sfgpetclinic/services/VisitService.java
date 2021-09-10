954152222a31769fd4e1301eb0e466c289d3a9db
Suprememajor <ndipnobertetta@gmail.com>
2021-09-10 18:44:36 +0100

    Implemented Map Impl for Visit service. Closes #33

---
+++ src/main/java/com/supremecorp/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package com.supremecorp.sfgpetclinic.services;
+
+import com.supremecorp.sfgpetclinic.models.Visit;
+
+/**
+ * Created by suprememajor on the 9/10/21
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
