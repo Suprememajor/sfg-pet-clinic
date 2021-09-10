f537c00c631ae8ebec6ca5387f14c2ccdd21825c
Suprememajor <ndipnobertetta@gmail.com>
2021-09-10 17:59:42 +0100

    Created Spring Data JPA Service for Owners. Closes #25

--- src/main/java/com/supremecorp/sfgpetclinic/repositories/OwnerRepository.java
+++ src/main/java/com/supremecorp/sfgpetclinic/repositories/OwnerRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by suprememajor on the 9/10/21
  */
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
